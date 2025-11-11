f14d8c74f1347e1f8b3ac8c0e85dc6dd94dd3708
dilyanabodurova <diliana.bodurova@abv.bg>
2025-11-11 08:54:43 +0200

    bug fix for wrong rounding for time_diff

--- conditional_statements_exercises/lunch_break.py
+++ conditional_statements_exercises/lunch_break.py
@@ -7,12 +7,12 @@ lunch_time = break_duration / 8
 relax_time = break_duration / 4
 
 total_time_needed = lunch_time + relax_time + film_duration
-time_difference = total_time_needed - break_duration
+time_difference = break_duration - total_time_needed
 
 if total_time_needed > break_duration:
     print(
         f"You don't have enough time to watch {film}, "
-        f"you need {abs(math.ceil(time_difference))} more minutes.")
+        f"you need {abs(math.ceil(-time_difference))} more minutes.")
 else:
     print(
         f"You have enough time to watch {film} and left with {abs(math.ceil(time_difference))} minutes free time."
