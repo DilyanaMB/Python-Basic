749ef987a1cd6da29bc29d01af495a0c02bcb36b
dilyanabodurova <diliana.bodurova@abv.bg>
2025-11-12 07:33:10 +0200

    1

--- PB_more_exercises/01_pipes_in_pool.py
+++ PB_more_exercises/01_pipes_in_pool.py
@@ -6,13 +6,14 @@ hours = float(input())
 p1_filled = p1 * hours
 p2_filled = p2 * hours
 
-total_filled_percent = (p1_filled + p2_filled) / volume * 100
-p1_filled_percent = p1_filled / (total_filled_percent * 0.1)
-p2_filled_percent = p2_filled / (total_filled_percent * 0.1)
 total_litters = p1_filled + p2_filled
+total_filled_percent = (p1_filled + p2_filled) / volume * 100
 
-if (total_litters <= volume):
+if total_litters <= volume:
+    p1_filled_percent = p1_filled / total_litters * 100
+    p2_filled_percent = p2_filled / total_litters * 100
+
     print(f"The pool is {total_filled_percent:.2f}% full. Pipe 1: {p1_filled_percent:.2f}%. Pipe 2:"
           f" {p2_filled_percent :.2f}%.")
 else:
-    print(f"For {hours} hours the pool overflows with {total_litters - volume:.2f} liters.")
+    print(f"For {hours:.2f} hours the pool overflows with {total_litters - volume:.2f} liters.")
