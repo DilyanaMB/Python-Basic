cd13c9ab46abc257a049e2b389dc66f729817f71
dilyanabodurova <diliana.bodurova@abv.bg>
2025-11-14 15:59:23 +0200

    t

---
+++ conditional_statements_advanced_lab/08_cinema_tickets.py
@@ -0,0 +1,7 @@
+day = input()
+if day == 'Sunday' or day == 'Saturday':
+    print(16)
+elif day == 'Monday' or day == 'Tuesday' or day == 'Friday':
+    print(12)
+elif day == 'Wednesday' or day == 'Thursday':
+    print(14)
