61c5134c47f7b6ba02b25a42432f4e78a64ea5b2
dilyanabodurova <diliana.bodurova@abv.bg>
2025-11-16 07:58:37 +0200

    6

--- conditional_statements_advanced_exercises/06_operation_between_numbers.py
+++ conditional_statements_advanced_exercises/06_operation_between_numbers.py
@@ -1,15 +1,27 @@
-n1=int(input())
-n2=int(input())
-operator =input()
+n1 = int(input())
+n2 = int(input())
+operator = input()
+is_result_even = 'even'
 
 if operator == "+":
-    print(n1+n2)
+    if (n1 + n2) % 2 != 0:
+        is_result_even = 'odd'
+    print(f"{n1} + {n2} = {n1 + n2} - {is_result_even}")
 elif operator == "-":
-    print(n1-n2)
+    if (n1 - n2) % 2 != 0:
+        is_result_even = 'odd'
+    print(f"{n1} - {n2} = {n1 - n2} - {is_result_even}")
 elif operator == "*":
-    print(n1*n2)
+    if (n1 * n2) % 2 != 0:
+        is_result_even = 'odd'
+    print(f"{n1} * {n2} = {n1 * n2} - {is_result_even}")
 elif operator == "/":
-    if n2!=0:
-        print(n1/n2)
+    if n2 != 0:
+        print(f"{n1} / {n2} = {n1 / n2:.2f}")
+    else:
+        print(f"Cannot divide {n1} by zero")
+elif operator == "%":
+    if n2 != 0:
+        print(f"{n1} % {n2} = {n1 % n2}")
     else:
         print(f"Cannot divide {n1} by zero")
